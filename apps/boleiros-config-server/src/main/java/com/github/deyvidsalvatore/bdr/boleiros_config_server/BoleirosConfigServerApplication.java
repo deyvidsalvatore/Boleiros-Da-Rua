562ba3942a7bb9359fba5741b1d29e8e791c6032
Deyvid Santos <deyvidsantosdatascience@gmail.com>
2026-01-03 09:12:14 -0300

    feat(commons): Initial commons project

--- apps/boleiros-config-server/src/main/java/com/github/deyvidsalvatore/bdr/boleiros_config_server/BoleirosConfigServerApplication.java
+++ apps/boleiros-config-server/src/main/java/com/github/deyvidsalvatore/bdr/boleiros_config_server/BoleirosConfigServerApplication.java
@@ -1,4 +1,4 @@
-package com.github.deyvidsalvatore;
+package com.github.deyvidsalvatore.bdr.boleiros_config_server;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
